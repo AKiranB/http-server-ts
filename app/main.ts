d99ef6278b12247ec335c2e3961098ed7d55d3a4
Kiran Boyle <anthony.kiran.boyle@gmail.com>
2024-11-01 09:18:18 +0100

    respond with status code 200

--- app/main.ts
+++ app/main.ts
@@ -2,10 +2,13 @@ import * as net from "net";
 
 console.log("Logs from your program will appear here!");
 
+const port = 4221;
+
 const server = net.createServer((socket) => {
+  socket.write(`HTTP/1.1 200 OK\r\n\r\n`);
   socket.on("close", () => {
     socket.end();
   });
 });
 
-server.listen(4221, "localhost");
+server.listen(port, "localhost");
