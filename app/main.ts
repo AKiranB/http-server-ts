8c4d3303cff0f17c17b45803727dfb5083971dd5
Kiran Boyle <anthony.kiran.boyle@gmail.com>
2024-11-02 09:41:47 +0100

    x

--- app/main.ts
+++ app/main.ts
@@ -5,12 +5,6 @@ console.log("Logs from your program will appear here!");
 const port = 4221;
 
 const server = net.createServer((socket) => {
-  socket.write(`HTTP/1.1 200 OK\r\n`);
-  socket.write(`Content-Type: text/plain\r\n`);
-  socket.write(`Content-Length: 13\r\n`);
-  socket.write(`\r\n`);
-  socket.write(`Hello, World!`);
-
   socket.on("data", (data) => {
     const req = data.toString();
 
