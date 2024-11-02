393833a5bd8f5f6e4ebc597a23846e673b427dea
Kiran Boyle <anthony.kiran.boyle@gmail.com>
2024-11-02 09:12:24 +0100

    check for path

--- app/main.ts
+++ app/main.ts
@@ -5,10 +5,27 @@ console.log("Logs from your program will appear here!");
 const port = 4221;
 
 const server = net.createServer((socket) => {
-  socket.write(`HTTP/1.1 200 OK\r\n\r\n`);
-  socket.on("close", () => {
-    socket.end();
+  socket.write(`HTTP/1.1 200 OK\r\n`);
+  socket.write(`Content-Type: text/plain\r\n`);
+  socket.write(`Content-Length: 13\r\n`);
+  socket.write(`\r\n`);
+  socket.write(`Hello, World!`);
+
+  socket.on("data", (data) => {
+    const req = data.toString();
+
+    const [method, path] = req.split(" ");
+
+    if (path !== "/") {
+      return 404;
+    }
+
+    return 200;
   });
+
+  socket.end();
 });
 
-server.listen(port, "localhost");
+server.listen(port, "localhost", () => {
+  console.log(`Server is listening on http://localhost:${port}`);
+});
