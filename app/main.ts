0e0589a099c59247f405ac0d057b251cf7d7d0c7
Kiran Boyle <anthony.kiran.boyle@gmail.com>
2024-11-07 12:37:01 +0100

    x

--- app/main.ts
+++ app/main.ts
@@ -4,18 +4,33 @@ console.log("Logs from your program will appear here!");
 
 const port = 4221;
 
+enum StatusCode {
+  "OK" = "200 OK",
+  "NOT_FOUND" = "404 Not Found",
+}
+
+const createResponse = (statusCode: number, body: string) => {
+  return `HTTP/1.1 ${statusCode}\r\nContent-Type: text/plain\r\nContent-Length: ${Buffer.byteLength(
+    body
+  )}\r\n\r\n${body}`;
+};
+
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
-    const req = data.toString();
+    const request = data.toString();
+    const [, path] = request.split(" ");
+    const [, mainRoute, subRoute] = path.split("/");
 
-    const [, path] = req.split(" ");
+    let response;
 
-    console.log(path);
-
-    const response =
-      path === "/"
-        ? "HTTP/1.1 200 OK\r\n\r\n"
-        : "HTTP/1.1 404 Not Found\r\n\r\n";
+    if (path === "/") {
+      response = createResponse(200, "Hello, World!");
+    } else if (mainRoute === "echo" && subRoute) {
+      const body = `Echo: ${subRoute}`;
+      response = createResponse(200, body);
+    } else {
+      response = createResponse(404, "Not Found");
+    }
 
     socket.write(response);
     socket.end();
