05991ed1ce078bf9ae6018da196822f814bb52bf
Kiran Boyle <anthony.kiran.boyle@gmail.com>
2024-11-11 11:43:36 +0100

    organise

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,3 @@
-import { file } from "bun";
 import * as net from "net";
 import * as filePath from "path";
 
@@ -17,6 +16,69 @@ interface ParsedRequestBody {
   Accept: string;
   "User-Agent": string;
 }
+
+const handleFileRequest = async (fileName: string, socket: net.Socket) => {
+  let response;
+  try {
+    const fp = getFilePath(fileName);
+    const file = await require("fs").promises.readFile(fp);
+    response = createResponse({
+      statusCode: StatusCode.OK,
+      body: file,
+      contentType: "application/octet-stream",
+    });
+    socket.write(response);
+    socket.write(file);
+  } catch (err) {
+    response = createResponse({
+      statusCode: StatusCode.NOT_FOUND,
+      body: "File not found",
+    });
+    socket.write(response);
+  }
+};
+
+const server = net.createServer((socket) => {
+  socket.on("data", async (data) => {
+    const requestLines = data.toString().split("\r\n");
+    const parsedRequestBody = createParsedRequestBody(requestLines);
+    const path = parsedRequestBody.path;
+    const [, mainPath, subPath] = path.split("/");
+    let response;
+    let body;
+
+    switch (path) {
+      case "/":
+        response = createResponse({
+          statusCode: StatusCode.OK,
+          body: "Connected To server",
+        });
+        break;
+      case "/echo":
+        response = createResponse({ statusCode: StatusCode.OK, body: "echo" });
+        break;
+      case `/echo/${subPath}`:
+        body = subPath || "echo";
+        response = createResponse({ statusCode: StatusCode.OK, body: body });
+        break;
+      case "/user-agent":
+        body = parsedRequestBody["User-Agent"] || "User-Agent not found";
+        response = createResponse({ statusCode: StatusCode.OK, body: body });
+        break;
+      case `/files/${subPath}`:
+        await handleFileRequest(subPath, socket);
+        break;
+      default:
+        response = createResponse({
+          statusCode: StatusCode.NOT_FOUND,
+          body: "Not Found",
+        });
+        break;
+    }
+    socket.write(response ?? "");
+    socket.end();
+  });
+});
 
 const createResponse = ({
   statusCode,
@@ -49,72 +111,16 @@ const createParsedRequestBody = (requestLines: string[]) => {
   return obj as ParsedRequestBody;
 };
 
-const args = process.argv;
-const directoryFlagIndex = args.indexOf("--directory");
-const baseDirectory =
-  directoryFlagIndex !== -1 ? args[directoryFlagIndex + 1] : null;
+const getFilePath = (fileName: string) => {
+  const args = process.argv;
+  const directoryFlagIndex = args.indexOf("--directory");
+  const pathArgs =
+    directoryFlagIndex !== -1 ? args[directoryFlagIndex + 1] : null;
 
-const server = net.createServer((socket) => {
-  socket.on("data", async (data) => {
-    const requestLines = data.toString().split("\r\n");
+  if (!pathArgs) return;
 
-    const parsedRequestBody = createParsedRequestBody(requestLines);
-    const path = parsedRequestBody.path;
-    const [, mainPath, subPath] = path.split("/");
-    let response;
-    let body;
-
-    switch (path) {
-      case "/":
-        response = createResponse({
-          statusCode: StatusCode.OK,
-          body: "Connected To server",
-        });
-        break;
-      case "/echo":
-        response = createResponse({ statusCode: StatusCode.OK, body: "echo" });
-        break;
-      case `/echo/${subPath}`:
-        body = subPath || "echo";
-        response = createResponse({ statusCode: StatusCode.OK, body: body });
-        break;
-      case "/user-agent":
-        body = parsedRequestBody["User-Agent"] || "User-Agent not found";
-        response = createResponse({ statusCode: StatusCode.OK, body: body });
-        break;
-      case `/files/${subPath}`:
-        console.log("reacehed");
-        try {
-          if (!baseDirectory) return;
-
-          const fp = filePath.join(baseDirectory, subPath);
-          const file = await require("fs").promises.readFile(fp);
-          response = createResponse({
-            statusCode: StatusCode.OK,
-            body: file,
-            contentType: "application/octet-stream",
-          });
-          socket.write(response);
-          socket.write(file);
-        } catch (err) {
-          response = createResponse({
-            statusCode: StatusCode.NOT_FOUND,
-            body: "File not found",
-          });
-          socket.write(response);
-        }
-        break;
-      default:
-        response = createResponse({
-          statusCode: StatusCode.NOT_FOUND,
-          body: "Not Found",
-        });
-        break;
-    }
-    socket.write(response);
-    socket.end();
-  });
-});
+  return filePath.join(pathArgs, fileName);
+};
 
 server.listen(port, "localhost", () => {
   console.log(`Server is listening on http://localhost:${port}`);
