e72a1620e3fbc11a86ed7a38a4348d15e718de05
Kiran Boyle <anthony.kiran.boyle@gmail.com>
2025-07-22 17:52:03 +0200

    Add Accept encoding header

--- app/types.ts
+++ app/types.ts
@@ -1,20 +1,21 @@
 export enum StatusCode {
-  "OK" = "200 OK",
-  "NOT_FOUND" = "404 Not Found",
-  "UNAUTHORIZED" = "401 Unauthorized",
-  "INTERNAL_SERVER_ERROR" = "500 Internal Server Error",
-  "CREATED" = "201 Created",
+    "OK" = "200 OK",
+    "NOT_FOUND" = "404 Not Found",
+    "UNAUTHORIZED" = "401 Unauthorized",
+    "INTERNAL_SERVER_ERROR" = "500 Internal Server Error",
+    "CREATED" = "201 Created",
 }
 
 export interface ParsedRequestBody {
-  method: string;
-  path: string;
-  protocol: string;
-  Host: string;
-  Accept: string;
-  Authorization: string;
-  "Content-type": string;
-  "Content-Length": string;
-  "User-Agent": string;
-  body: string;
+    method: string;
+    path: string;
+    protocol: string;
+    Host: string;
+    Accept: string;
+    Authorization: string;
+    "Content-type": string;
+    "Content-Length": string;
+    "User-Agent": string;
+    "Accept-Encoding": string;
+    body: string;
 }
