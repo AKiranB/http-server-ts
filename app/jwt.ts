8d31531bffeecbb12c1c2b37699e2d4da3924cf3
Kiran Boyle <anthony.kiran.boyle@gmail.com>
2025-07-27 21:56:24 +0200

    comment out jwt

--- app/jwt.ts
+++ app/jwt.ts
@@ -1,21 +1,21 @@
-import jwt from "jsonwebtoken";
+// import jwt from "jsonwebtoken";
 
-const SECRET_KEY = "NOTSOSECRET";
+// const SECRET_KEY = "NOTSOSECRET";
 
-export const createToken = (
-  payload: Record<string, any>,
-  expiresIn: number
-) => {
-  return jwt.sign(payload, SECRET_KEY, {
-    expiresIn,
-  });
-};
+// export const createToken = (
+//   payload: Record<string, any>,
+//   expiresIn: number
+// ) => {
+//   return jwt.sign(payload, SECRET_KEY, {
+//     expiresIn,
+//   });
+// };
 
-export const verifyToken = ({ token }: { token: string }) => {
-  try {
-    return jwt.verify(token, SECRET_KEY);
-  } catch (err) {
-    console.log(err);
-    return null;
-  }
-};
+// export const verifyToken = ({ token }: { token: string }) => {
+//   try {
+//     return jwt.verify(token, SECRET_KEY);
+//   } catch (err) {
+//     console.log(err);
+//     return null;
+//   }
+// };
