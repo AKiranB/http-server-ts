cc68e1f8c17418f95ea0467da39d66cbe9193676
Kiran Boyle <anthony.kiran.boyle@gmail.com>
2025-08-03 13:04:40 +0200

    Simplify main

--- app/main.ts
+++ app/main.ts
@@ -1,145 +1,19 @@
 import * as net from "net";
-import { StatusCode, type ParsedRequestBody } from "./types";
 import Responder from "./responder";
-
-const port = 4221;
-
-const acceptedEncodings = ["gzip", "deflate", "br", "identity"];
-
-const getEncodingType = (clientEncodingHeader: string) => {
-    if (acceptedEncodings.includes(clientEncodingHeader)) {
-        console.log("true");
-        return clientEncodingHeader;
-    }
-    return null;
-};
-
-const handleResponse = (
-    req: ReturnType<typeof parseRequestBody>,
-    res: Responder,
-    subPath: string
-) => {
-    switch (req.path) {
-        case "/":
-            res.send({ statusCode: StatusCode.OK });
-            break;
-
-        case "/echo":
-            res.send({ statusCode: StatusCode.OK, body: "echo" });
-            return;
-
-        case `/echo/${subPath}`:
-            res.send({
-                statusCode: StatusCode.OK,
-                body: subPath || "echo",
-                encodingType: getEncodingType(req["Accept-Encoding"])
-                    ? req["Accept-Encoding"]
-                    : undefined,
-            });
-            return;
-
-        case "/user-agent":
-            res.send({
-                statusCode: StatusCode.OK,
-                body: req["User-Agent"] || "User-Agent not found",
-            });
-            return;
-
-        case `/files/${subPath}`:
-            res.handleFileRequest({
-                fileName: subPath,
-                method: req.method,
-                body: req.body,
-            });
-            return;
-
-        // case "/login":
-        //     const { body } = req || {};
-        //     const parsedBody = JSON.parse(body);
-        //     const { username, password } = parsedBody;
-        //     if (username === "testuser" && password === "password123") {
-        //         res.send({
-        //             statusCode: StatusCode.OK,
-        //             body: token,
-        //             contentType: "application/json",
-        //         });
-        //     } else {
-        //         res.send({
-        //             statusCode: StatusCode.NOT_FOUND,
-        //             body: "Login Failed",
-        //         });
-        //     }
-        //     break;
-
-        // case "/protected":
-        //     const authHeader = req.Authorization;
-        //     const token = authHeader.split(" ")[1];
-        //     const decodedToken = verifyToken({ token });
-
-        //     if (decodedToken) {
-        //         res.send({
-        //             statusCode: StatusCode.OK,
-        //             body: "...mock-Protected Data",
-        //         });
-        //     } else {
-        //         res.send({
-        //             statusCode: StatusCode.UNAUTHORIZED,
-        //             body: "Unauthorized",
-        //         });
-        //     }
-        //     break;
-
-        default:
-            res.send({
-                statusCode: StatusCode.NOT_FOUND,
-                body: "Not Found",
-            });
-            break;
-    }
-};
-
-const getInformationFromRequest = ({ request }: { request: Buffer }) => {
-    const requestLines = request.toString().split("\r\n");
-    requestLines;
-    const req = parseRequestBody(requestLines);
-    const path = req.path;
-    const subPath = path.split("/")?.[2];
-
-    return {
-        req,
-        subPath,
-    };
-};
+import { PORT } from "./constants";
+import { getInformationFromRequest } from "./utils";
+import handleResponse from "./handleResponse";
 
 const server = net.createServer((socket) => {
     socket.on("data", async (data) => {
         const { req, subPath } = getInformationFromRequest({
             request: data,
         });
-
         const res = new Responder(socket);
         handleResponse(req, res, subPath);
     });
 });
 
-const parseRequestBody = (requestLines: string[]) => {
-    const obj: Record<string, any> = {};
-    requestLines.forEach((line: string, index) => {
-        if (index === 0) {
-            const [method, path, protocol] = line.split(" ");
-            obj["method"] = method;
-            obj["path"] = path;
-            obj["protocol"] = protocol;
-        }
-        if (index === requestLines.length - 1) {
-            return (obj["body"] = line);
-        }
-        const [key, value] = line.split(": ");
-        if (key && value) obj[key] = value;
-    });
-    return obj as ParsedRequestBody;
-};
-
-server.listen(port, "localhost", () => {
-    console.log(`Server is listening on http://localhost:${port}`);
+server.listen(PORT, "localhost", () => {
+    console.log(`Server is listening on http://localhost:${PORT}`);
 });
